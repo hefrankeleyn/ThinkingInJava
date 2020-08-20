a2f6c4ce6ff8f29e0235e183162538626d6a0a3b
Li Fei <hefrankeleyn@gamil.com>
2020-08-20 20:57:57 +0800

    初始化仓库
    对ThinkingInJava的仓库进行初始化

---
+++ thinkingInJavaPro/src/main/java/com/hef/chapter3/OperatorBus.java
@@ -0,0 +1,15 @@
+package com.hef.chapter3;
+
+/**
+ * @author lifei
+ * @since 2020/8/18
+ */
+public class OperatorBus {
+
+    public static void main(String[] args) {
+        float f4 = 1e-43f;
+        double d01 = 2e+3;
+        System.out.println(f4);
+        System.out.println(d01);
+    }
+}
